aabc95307a7373a86386b9cf50ac88b91bf4286f
SUXUING-star <jieyalinjiu@gmail.com>
2026-02-04 22:11:50 +0800

    up

--- vite.config.ts
+++ vite.config.ts
@@ -26,27 +26,6 @@ export default defineConfig({
           'vendor-markdown': ['marked'],
           // 动画相关
           'vendor-anime': ['animejs'],
-          // 工具函数 (假设你以后会把它们改成 .ts)
-          'utils': [
-            './src/utils/markdownUtils.ts',
-            './src/utils/imageUtils.ts',
-            './src/utils/postUtils.ts'
-          ],
-          // 布局组件 (假设你以后会把它们改成 .tsx)
-          'layout': [
-            './src/components/layout/Header.tsx',
-            './src/components/layout/Footer.tsx',
-            './src/components/layout/LeftSidebar.tsx',
-            './src/components/layout/RightSidebar.tsx',
-            './src/components/layout/BackgroundLayout.tsx'
-          ],
-          // 通用组件 (假设你以后会把它们改成 .tsx)
-          'common': [
-            './src/components/common/LoadingSpinner.tsx',
-            './src/components/common/Broadcast.tsx',
-            './src/components/common/ProgressBar.tsx',
-            './src/components/common/SplashScreen.tsx'
-          ]
         }
       }
     },
